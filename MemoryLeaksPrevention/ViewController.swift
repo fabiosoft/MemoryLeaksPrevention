5f63a3b4e9a1ab7cfdeb6e2f7cc8007085336223
Fabio Nisci <fabionisci@gmail.com>
2022-05-13 23:50:29 +0200

    Initial Commit

---
+++ MemoryLeaksPrevention/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MemoryLeaksPrevention
+//
+//  Created by Fabio Nisci on 13/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
